564190a038db9d409cbadf36626ae27bbc70924e
marcusbullock <marcus@theadventurepeople.com>
2024-06-13 10:34:25 +0100

    added components and styling with tailwind

---
+++ src/ui/Header.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
+import Username from '../features/user/Username';
+
+function Header() {
+    return (
+        <header className="border=stone-200 flex items-center justify-between border-b bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+            <Link
+                to="/"
+                className="flex items-center gap-3 text-xl tracking-widest"
+            >
+                <div className="rounded-full bg-purple-200 p-1">🍕</div>
+                Fatboy&apos;s Pizza Co.
+            </Link>
+            <SearchOrder />
+            <Username />
+        </header>
+    );
+}
+
+export default Header;
